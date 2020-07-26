07a79b623be13285e47dd11205905e19781e4673
Ugyen Norbu <unorbu2019@gmail.com>
2020-07-26 22:33:30 +0600

    with fwifld cardff

--- slot_machine.R
+++ slot_machine.R
@@ -1,9 +1,14 @@
+# SOT MACINE --------------------------------------------------------------
+
 get_symbols <- function() {
     icons <- c("DD", "7", "BBB", "BB", "B", "C", "0")
     sample(icons, size = 3, replace = TRUE, 
            prob = c(0.03, 0.03, 0.06, 0.1, 0.25, 0.01, 0.52))
 }
-
+a <- get_symbols()
+score(a)
+a
+str(a)
 score <- function(symbols){
     # Identify cases
     same <- length(unique(symbols)) == 1
@@ -52,4 +57,72 @@ print.slots <- function(x, ...) {
     slot_display(x)
 }
 
-play()
+# SIMULATION --------------------------------------------------------------
+library(tidyverse)
+
+wheel <- c("DD", "7", "BBB", "BB", "B", "C", "0")
+
+combos <- expand.grid(wheel, wheel, wheel, stringsAsFactors = FALSE)
+
+prob <- c("DD" = 0.03, "7" = 0.03, "BBB" = 0.06, "BB" = 0.1, 
+          "B" = 0.25, "C" = 0.01, "0" = 0.52)
+combos <- combos %>% 
+    mutate(prob_1 = prob[Var1]) %>% 
+    mutate(prob_2 = prob[Var2]) %>% 
+    mutate(prob_3 = prob[Var3]) %>% 
+    mutate(prob = prob_1*prob_2*prob_3) %>% 
+    mutate(prize = NA)
+
+for (i in 1:nrow(combos)){
+    sym <-  c(combos[i, 1], combos[i, 2], combos[i, 3])
+    combos$prize[i] <- score(sym)
+}
+
+combos <- combos %>% 
+    mutate(exp_val = prob*prize)
+
+sum(combos$exp_val)
+
+
+# With Wild Card ----------------------------------------------------------
+
+score_wild <- function(symbols) { 
+    diamonds <- sum(symbols == "DD")
+    cherries <- sum(symbols == "C")
+    # identify case
+    # since diamonds are wild, only nondiamonds matter for three of a kind and all bars 
+    slots <- symbols[symbols != "DD"]
+    same <- length(unique(slots)) == 1
+    bars <- slots %in% c("B", "BB", "BBB")
+    # assign prize
+    if (diamonds == 3) { 
+        prize <- 100
+    } else if (same) {
+        payouts <- c("7" = 80, "BBB" = 40, "BB" = 25,
+                     "B"=10,"C"=10,"0"=0) 
+        prize <- unname(payouts[slots[1]])
+    } else if (all(bars)) { 
+        prize <- 5
+    } else if (cherries > 0) {
+        # diamonds count as cherries
+        # so long as there is one real cherry
+        prize <- c(0, 2, 5)[cherries + diamonds + 1]
+    }else{ 
+        prize <- 0
+    }
+    # double for each diamond
+    prize * 2^diamonds
+}
+
+combos <- combos %>% 
+    mutate(prize_wild  = NA)
+
+for (i in 1:nrow(combos)){
+    sym <-  c(combos[i, 1], combos[i, 2], combos[i, 3])
+    combos$prize_wild[i] <- score_wild(sym)
+}
+
+combos <- combos %>% 
+    mutate(exp_val_wild = prob*prize_wild)
+
+sum(combos$exp_val_wild)
